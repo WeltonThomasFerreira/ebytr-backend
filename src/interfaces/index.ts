d7a0e05bf36c6f59ae919b32139de41939836b22
Thomas Ferreira <weltonthomasferreira@outlook.com>
2022-05-20 00:56:01 -0300

    feat(api): add index for interfaces

---
+++ src/interfaces/index.ts
@@ -0,0 +1,7 @@
+import { Controller } from './controller'
+import { Model } from './model'
+import { Response } from './response'
+import { Service } from './service'
+import { Task } from './task'
+
+export { Controller, Model, Response, Service, Task }
