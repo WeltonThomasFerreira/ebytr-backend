6c0d446b507c1aee5f4d603e441c8d8ed8a4cae1
Thomas Ferreira <weltonthomasferreira@outlook.com>
2022-05-20 03:08:53 -0300

    test: add find all tasks controller

--- src/tests/controllers/controller.test.ts
+++ src/tests/controllers/controller.test.ts
@@ -3,9 +3,10 @@ import { expect } from 'chai';
 import * as sinnon from 'sinon';
 
 import AddNewTaskController from '../../controllers/addNewTaskController';
+import FindAllTasksController from '../../controllers/findAllTasksController';
 import TaskModel from '../../models/taskModel';
 import TaskService from '../../services/taskService';
-import { addNewTask } from './mocks';
+import { addNewTask, findAllTasks } from './mocks';
 
 const taskModel = new TaskModel();
 const taskService = new TaskService();
@@ -40,6 +41,50 @@ describe('Add new task', () => {
 
   it('Return server error', async () => {
     createStub.throwsException('Server Error');
+    const response = await sut.handle(request);
+    expect(response.statusCode).equal(500);
+  });
+});
+
+describe('Find all tasks', () => {
+  const { finded, alpha, newest } = findAllTasks;
+  const sut = new FindAllTasksController(taskModel);
+  const readStub = sinnon.stub(taskModel, 'read');
+
+  beforeEach(async () => {
+    readStub.resolves(finded);
+  });
+
+  after(async () => {
+    readStub.restore();
+  });
+
+  it('Return tasks successfully', async () => {
+    const request = { query: {} };
+    const response = await sut.handle(request);
+    expect(response.statusCode).equal(200);
+    expect(response.body).deep.equal(finded);
+  });
+
+  it('Return tasks in alphabetic order', async () => {
+    readStub.resolves(alpha);
+    const request = { query: { sort: 'alpha' } };
+    const response = await sut.handle(request);
+    expect(response.statusCode).equal(200);
+    expect(response.body).deep.equal(alpha);
+  });
+
+  it('Return tasks in order of most recent', async () => {
+    readStub.resolves(newest);
+    const request = { query: { sort: 'new' } };
+    const response = await sut.handle(request);
+    expect(response.statusCode).equal(200);
+    expect(response.body).deep.equal(newest);
+  });
+
+  it('Return server error', async () => {
+    const request = { query: {} };
+    readStub.throwsException('Server Error');
     const response = await sut.handle(request);
     expect(response.statusCode).equal(500);
   });
